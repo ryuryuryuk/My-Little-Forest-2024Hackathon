6137853fb31f4a4e8e3177cdcca847419275aefb
Arin0303 <cindy030225@gmail.com>
2024-11-22 01:26:32 +0900

    media setting

--- mylittleforest_server/mysite/settings.py
+++ mylittleforest_server/mysite/settings.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -136,3 +136,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+#미디어 파일 저장 위치
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- mylittleforest_server/networking/views.py
+++ mylittleforest_server/networking/views.py
@@ -4,7 +4,22 @@ from start.models import User
 
 def networking(request):
     return render(request, 'networking/networking.html') 
+
 def makeGroup(request):
+    if request.method == 'POST':
+        #텍스트 데이터 받기
+        tag1 = request.POST.get('tag1')
+        tag2 = request.POST.get('tag2')
+        tag3 = request.POST.get('tag3')
+        group_name = request.POST.get('group-name')
+        group_text = request.POST.get('group_text')
+        # 파일 데이터 받기
+        group_img = request.FILES.get('group-img')
+        # 디버그 출력
+        print(f"tag1: {tag1}, tag2: {tag2}, tag3: {tag3}")
+        print(f"group_name: {group_name}, group_text: {group_text}")
+        print(f"group_img: {group_img}")
     return render(request, 'networking/makeGroup.html') 
+
 def tagSearch(request):
     return render(request, 'networking/tagSearch.html') 
