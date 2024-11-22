83ef53bd1595b664f6cb48db5ac4649a016bbd80
Arin0303 <cindy030225@gmail.com>
2024-11-23 00:37:54 +0900

    sharingContents기능구현

--- mylittleforest_server/sharing/urls.py
+++ mylittleforest_server/sharing/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
 
 app_name = "sharing"
 
 urlpatterns = [
-    path('',views.sharingContents2, name='sharingContents2'),
-    path('writeContents/',views.writeContents, name='writeContents'),
-    path('writeContents_logic/',views.writeContents_logic, name='writeContents_logic'),
+    path('',views.sharingContents, name='sharingContents'),
+    path('post/<int:pk>/', views.postpage, name='postpage'),  # 게시글 상세 페이지
+    path('writeContents/',views.get_writeContents, name='writeContents'),
+    path('writeContents_logic/', views.writeContents_logic, name='writeContents_logic'),
+    path('myContents/',views.myContents, name='myContents'),
 
-    # path('mycontents/',views.mycontents, name='mycontents'),
     # path('sharingContents/',views.sharingContents, name='sharingContents'),
     # path('postPage/',views.postPage, name='postPage'),
     # path('press_like/',views.press_like, name='press_like'),
